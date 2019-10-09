b04ae30bbbb370596fe45bd146e8dcd3a37dc0fa
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-09 22:52:59 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,3 @@
-def say_hello(name)
-  puts "Hello #{name}!"
-end
-  def say_hello(name= "Ruby Programmer")
-    puts "Hello, #{name}"
-  end
+def greeting(name="Ruby programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
